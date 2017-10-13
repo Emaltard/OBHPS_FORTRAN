c40141243002a61949256eb0b677094a80a08883
Emaltard <maxence.joulin@gmail.com>
2017-10-13 11:53:44 +0200

    Fin TD1

--- TD1/exo6.f90
+++ TD1/exo6.f90
@@ -8,20 +8,22 @@ PROGRAM test
   READ *, s1
   PRINT *, "Donnez s2:"
   READ *, s2
-  PRINT *, occ_c(s1,'a')
+  CALL concatenation(s1,s2)
+  PRINT *, occ_c(s1, 'c')
 CONTAINS
   SUBROUTINE concatenation (s1,s2)
-    CHARACTER (len=100) :: s1,s2
-    PRINT *, TRIM(s1)//TRIM(s2)
+    CHARACTER (len=*) :: s1,s2
+    PRINT *, TRIM(ADJUSTL(s1))//TRIM(s2)
   END SUBROUTINE concatenation
 
   INTEGER FUNCTION occ_c (s1,c)
-    CHARACTER (len=50) :: s1
-    CHARACTER :: c
-    INTEGER :: i, compteur = 0
+    CHARACTER (len=*) :: s1
+    CHARACTER, INTENT(in) :: c
+    INTEGER :: i
+    occ_c = 0
 
-    DO i = 1, 50, 1
-       IF(s1(i)==c) compteur = compteur +1
+    DO i = 1, LEN(s1), 1
+       IF(s1(i:i)==c) occ_c = occ_c +1
     END DO
 
   END FUNCTION occ_c
